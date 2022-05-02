92c89f6aafe354a2494202dd550543e48c126c60
marweumr <marcus.wallau@googlemail.com>
2022-05-02 12:24:17 +0200

    test

--- build.rs
+++ build.rs
@@ -60,7 +60,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_args(&["-x", "c++", "-std=c++11"])
-        .clang_arg(format!("-I{}", xgb_root.join("include/xgboost").display()))
+        .clang_arg(format!("-I{}", xgb_root.join("include").display()))
         .clang_arg(format!("-I{}", xgb_root.join("rabit/include").display()))
         .clang_arg(format!(
             "-I{}",
