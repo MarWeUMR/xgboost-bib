f459a402a2d2f0f9ac1f9bd75ff766bc578b8833
marweumr <marcus.wallau@googlemail.com>
2022-05-24 16:46:30 +0200

    final fix (hopefully)

--- build.rs
+++ build.rs
@@ -19,6 +19,10 @@ fn main() {
         .clang_args(&["-x", "c++", "-std=c++11"])
         .clang_arg(format!(
             "-I{}",
+            Path::new(&String::from("xgboost/include")).display()
+        ))
+        .clang_arg(format!(
+            "-I{}",
             Path::new(&String::from("xgboost/rabit/include")).display()
         ))
         .clang_arg(format!(
